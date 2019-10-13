46f8c9642f62907bc559fab556e244bdc9310f78
Masaru Tsuchiyama <m.tmatma@gmail.com>
2019-10-13 20:19:08 +0900

    yocto ビルド用のスクリプトを追加

---
+++ build-yocto.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+cd poky
+source oe-init-build-env build-rpi
+bitbake-layers remove-layer meta-yocto-bsp
+bitbake-layers add-layer ../meta-openembedded/meta-oe
+bitbake-layers add-layer ../meta-openembedded/meta-multimedia
+bitbake-layers add-layer ../meta-openembedded/meta-python
+bitbake-layers add-layer ../meta-openembedded/meta-networking
+bitbake-layers add-layer ../meta-qt5
+bitbake-layers add-layer ../meta-raspberrypi
+bitbake-layers add-layer ../meta-rpi
+
+cat $SCRIPT_DIR/extra-local.conf >> conf/local.conf
+
+bitbake console-image
+
