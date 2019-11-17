ecb3e697e9dc504c78711ce716a7dfc9a1074a4c
Masaru Tsuchiyama <m.tmatma@gmail.com>
2019-11-17 09:34:56 +0900

    remove tag name.

--- raspberry-pi/yocto-build.sh
+++ raspberry-pi/yocto-build.sh
@@ -9,6 +9,6 @@ TARGET_OPT=/opt
 
 mkdir -p $DOCKER_HOME
 mkdir -p $DOCKER_OPT
-sudo docker run --rm -u yocto:yocto -v $DOCKER_OPT:$TARGET_OPT -v $DOCKER_HOME:$TARGET_HOME -w $TARGET_HOME yocto-raspberry:latest $TARGET_HOME/build-yocto.sh
+sudo docker run --rm -u yocto:yocto -v $DOCKER_OPT:$TARGET_OPT -v $DOCKER_HOME:$TARGET_HOME -w $TARGET_HOME yocto-raspberry $TARGET_HOME/build-yocto.sh
 
 
