6849f6e534726b89701ee1bea779002e910a68ff
Masaru Tsuchiyama <m.tmatma@gmail.com>
2019-11-24 14:43:57 +0900

    URL の間違いを修正

--- raspberry-pi/clone.sh
+++ raspberry-pi/clone.sh
@@ -3,7 +3,7 @@
 BRANCHNAME=zeus
 ROOTDIR=container/yocto/poky
 
-git clone  -b $BRANCHNAME  https://github.com/yocto-mirror-raspberrypi/pocky.git               $ROOTDIR
+git clone  -b $BRANCHNAME  https://github.com/yocto-mirror-raspberrypi/poky.git                $ROOTDIR
 git clone  -b $BRANCHNAME  https://github.com/yocto-mirror-raspberrypi/meta-openembedded.git   $ROOTDIR/meta-openembedded
 git clone  -b $BRANCHNAME  https://github.com/yocto-mirror-raspberrypi/meta-raspberrypi.git    $ROOTDIR/meta-raspberrypi
 git clone  -b $BRANCHNAME  https://github.com/meta-qt5/meta-qt5.git                            $ROOTDIR/meta-qt5
