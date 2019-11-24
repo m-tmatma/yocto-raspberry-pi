8eb3333fcb9dd69a33ee42a2f86e135014c058c2
Masaru Tsuchiyama <m.tmatma@gmail.com>
2019-11-24 09:47:21 +0900

    mirror を指定する

--- raspberry-pi/clone.sh
+++ raspberry-pi/clone.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -e
 
 BRANCHNAME=zeus
-cd container/yocto
-git clone  -b $BRANCHNAME  git://git.yoctoproject.org/poky
+ROOTDIR=container/yocto/poky
 
-cd poky
-git clone  -b $BRANCHNAME  git://git.openembedded.org/meta-openembedded
-git clone  -b $BRANCHNAME  git://git.yoctoproject.org/meta-raspberrypi
-git clone  -b $BRANCHNAME  git://github.com/meta-qt5/meta-qt5.git
-git clone  -b $BRANCHNAME  https://github.com/jumpnow/meta-rpi.git
+git clone  -b $BRANCHNAME  https://github.com/yocto-mirror-raspberrypi/pocky.git               $ROOTDIR
+git clone  -b $BRANCHNAME  https://github.com/yocto-mirror-raspberrypi/meta-openembedded.git   $ROOTDIR/meta-openembedded
+git clone  -b $BRANCHNAME  https://github.com/yocto-mirror-raspberrypi/meta-raspberrypi.git    $ROOTDIR/meta-raspberrypi
+git clone  -b $BRANCHNAME  https://github.com/meta-qt5/meta-qt5.git                            $ROOTDIR/meta-qt5
+git clone  -b $BRANCHNAME  https://github.com/jumpnow/meta-rpi.git                             $ROOTDIR/meta-rpi
 
