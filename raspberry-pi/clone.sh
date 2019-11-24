9c0291796aea1536325f3f967d5b9501b49491ed
Masaru Tsuchiyama <m.tmatma@gmail.com>
2019-11-24 09:52:24 +0900

    upstream を追加

--- raspberry-pi/clone.sh
+++ raspberry-pi/clone.sh
@@ -9,3 +9,5 @@ git clone  -b $BRANCHNAME  https://github.com/yocto-mirror-raspberrypi/meta-rasp
 git clone  -b $BRANCHNAME  https://github.com/meta-qt5/meta-qt5.git                            $ROOTDIR/meta-qt5
 git clone  -b $BRANCHNAME  https://github.com/jumpnow/meta-rpi.git                             $ROOTDIR/meta-rpi
 
+pushd  $ROOTDIR                    &&  git remote add upstream git://git.openembedded.org/meta-openembedded  && popd
+pushd  $ROOTDIR/meta-openembedded  &&  git remote add upstream git://git.yoctoproject.org/meta-raspberrypi   && popd
