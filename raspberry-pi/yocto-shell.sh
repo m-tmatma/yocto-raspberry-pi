e68d6d4a51e4a88b369a95f3ba6e65b1c69ac312
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-03-28 15:35:27 +0900

    yocto 用の shell を修正

--- raspberry-pi/yocto-shell.sh
+++ raspberry-pi/yocto-shell.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
-docker run -it -u yocto:yocto -v $(pwd):$(pwd) -w $(pwd) yocto-raspberry /bin/bash
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+DOCKER_HOME=$SCRIPT_DIR/container/yocto
+TARGET_HOME=/home/yocto
+
+DOCKER_OPT=$SCRIPT_DIR/container/opt
+TARGET_OPT=/opt
+
+mkdir -p $DOCKER_HOME
+mkdir -p $DOCKER_OPT
+docker run -it -u yocto:yocto -v $DOCKER_OPT:$TARGET_OPT -v $DOCKER_HOME:$TARGET_HOME -w $TARGET_HOME yocto-raspberry /bin/bash
+
 
