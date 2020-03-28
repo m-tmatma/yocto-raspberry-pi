52adac1b2de071c6789a29d11edbbbf93ca5f334
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-03-29 08:57:20 +0900

    toolchain をinstall

--- raspberry-pi/container/yocto/build-yocto.sh
+++ raspberry-pi/container/yocto/build-yocto.sh
@@ -22,8 +22,8 @@ ln -sf   $SCRIPT_DIR/downloads
 bitbake meta-ide-support
 bitbake meta-toolchain
 
-#PASSWORD=yocto
-#find ./tmp/deploy/sdk -name *.sh | xargs -n 1 -I "{}" sh -c "echo ${PASSWORD} | sudo -S {} -y"
+PASSWORD=yocto
+find ./tmp/deploy/sdk -name *.sh | xargs -n 1 -I "{}" sh -c "echo ${PASSWORD} | sudo -S {} -y"
 
 bitbake core-image-full-cmdline
 
