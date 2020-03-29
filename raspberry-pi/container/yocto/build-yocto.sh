858030df65a7b32c8b3df76a033e240e1ea81407
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-03-29 20:03:47 +0900

    sudo で実行するのをやめる

--- raspberry-pi/container/yocto/build-yocto.sh
+++ raspberry-pi/container/yocto/build-yocto.sh
@@ -22,8 +22,7 @@ ln -sf   $SCRIPT_DIR/downloads
 bitbake meta-ide-support
 bitbake meta-toolchain
 
-PASSWORD=yocto
-find ./tmp/deploy/sdk -name *.sh | xargs -n 1 -I "{}" sh -c "echo ${PASSWORD} | sudo -S {} -y"
+find ./tmp/deploy/sdk -name *.sh | xargs -n 1 -I "{}" sh -c "{} -y"
 
 #bitbake core-image-full-cmdline
 bitbake console-image
