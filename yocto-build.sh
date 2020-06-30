6820e6933ebe33345a66de5db0a833c8027a702c
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-06-30 21:51:23 +0900

    image をリスト表示する

--- yocto-build.sh
+++ yocto-build.sh
@@ -11,5 +11,5 @@ mkdir -p $DOCKER_HOME
 mkdir -p $DOCKER_OPT
 docker run --rm -u yocto:yocto -v $DOCKER_OPT:$TARGET_OPT -v $DOCKER_HOME:$TARGET_HOME -w $TARGET_HOME yocto-raspberry $TARGET_HOME/build-yocto.sh
 
-ls -Ll $DOCKER_HOME/poky/build-rpi/tmp/deploy/images/raspberrypi4/console-image-raspberrypi4.rpi-sdimg
+ls -Ll $DOCKER_HOME/poky/build-rpi/tmp/deploy/images/raspberrypi4/*-image-*
 
