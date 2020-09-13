8e70765ca43c39218e4927461c5041310cd3a7be
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-09-13 16:33:17 +0900

    abort if error.

--- yocto-build.sh
+++ yocto-build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 DOCKER_HOME=$SCRIPT_DIR/container/home/yocto
@@ -9,7 +9,10 @@ TARGET_OPT=/opt
 
 mkdir -p $DOCKER_HOME
 mkdir -p $DOCKER_OPT
-docker run --rm -u yocto:yocto -v $DOCKER_OPT:$TARGET_OPT -v $DOCKER_HOME:$TARGET_HOME -w $TARGET_HOME yocto-raspberry $TARGET_HOME/build-yocto.sh
+docker run --rm -u yocto:yocto \
+	-v $DOCKER_OPT:$TARGET_OPT \
+	-v $DOCKER_HOME:$TARGET_HOME \
+	-v /etc/apt/apt.conf:/etc/apt/apt.conf -w $TARGET_HOME yocto-raspberry $TARGET_HOME/build-yocto.sh
 
 ls -Ll $DOCKER_HOME/poky/build-rpi/tmp/deploy/images/raspberrypi4/*-image-*
 
