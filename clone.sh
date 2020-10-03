211d94a0721315074ac120da980fd36513a79de3
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-10-04 08:18:06 +0900

    change layout.

--- clone.sh
+++ clone.sh
@@ -1,14 +1,10 @@
 #!/bin/bash -e
 
 BRANCHNAME=warrior
-ROOTDIR=container/home/yocto/poky
+ROOTDIR=container/home/yocto
 
-git clone  -b $BRANCHNAME  https://github.com/yocto-mirror-raspberrypi/poky.git                $ROOTDIR
+git clone  -b $BRANCHNAME  https://github.com/yocto-mirror-raspberrypi/poky.git                $ROOTDIR/poky
 git clone  -b $BRANCHNAME  https://github.com/openembedded/meta-openembedded.git               $ROOTDIR/meta-openembedded
 git clone  -b $BRANCHNAME  https://github.com/yocto-mirror-raspberrypi/meta-raspberrypi.git    $ROOTDIR/meta-raspberrypi
 git clone  -b $BRANCHNAME  https://github.com/meta-qt5/meta-qt5.git                            $ROOTDIR/meta-qt5
 git clone  -b $BRANCHNAME  https://github.com/jumpnow/meta-rpi.git                             $ROOTDIR/meta-rpi
-
-pushd  $ROOTDIR                    &&  git remote add upstream git://git.yoctoproject.org/poky               && popd
-pushd  $ROOTDIR/meta-raspberrypi   &&  git remote add upstream git://git.yoctoproject.org/meta-raspberrypi   && popd
-pushd  $ROOTDIR/meta-openembedded  &&  git remote add upstream git://git.openembedded.org/meta-openembedded  && popd
--- container/home/yocto/build-yocto.sh
+++ container/home/yocto/build-yocto.sh
@@ -2,16 +2,15 @@
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-cd poky
-source oe-init-build-env build-rpi
-#bitbake-layers remove-layer meta-yocto-bsp
-bitbake-layers add-layer ../meta-openembedded/meta-oe
-bitbake-layers add-layer ../meta-openembedded/meta-python
-bitbake-layers add-layer ../meta-openembedded/meta-multimedia
-bitbake-layers add-layer ../meta-openembedded/meta-networking
-bitbake-layers add-layer ../meta-qt5
-bitbake-layers add-layer ../meta-raspberrypi
-bitbake-layers add-layer ../meta-rpi
+source $SCRIPT_DIR/poky/oe-init-build-env $SCRIPT_DIR/build-rpi
+#bitbake-layers remove-layer $SCRIPT_DIR/meta-yocto-bsp
+bitbake-layers add-layer $SCRIPT_DIR/meta-openembedded/meta-oe
+bitbake-layers add-layer $SCRIPT_DIR/meta-openembedded/meta-python
+bitbake-layers add-layer $SCRIPT_DIR/meta-openembedded/meta-multimedia
+bitbake-layers add-layer $SCRIPT_DIR/meta-openembedded/meta-networking
+bitbake-layers add-layer $SCRIPT_DIR/meta-qt5
+bitbake-layers add-layer $SCRIPT_DIR/meta-raspberrypi
+bitbake-layers add-layer $SCRIPT_DIR/meta-rpi
 
 cat $SCRIPT_DIR/extra-local.conf >> conf/local.conf
 mkdir -p $SCRIPT_DIR/downloads
@@ -22,7 +21,7 @@ ln -sf   $SCRIPT_DIR/downloads
 bitbake meta-ide-support
 bitbake meta-toolchain
 
-find ./tmp/deploy/sdk -name *.sh | xargs -n 1 -I "{}" sh -c "{} -y"
+find $SCRIPT_DIR/build-rpi/tmp/deploy/sdk -name *.sh | xargs -n 1 -I "{}" sh -c "{} -y"
 
 #bitbake core-image-full-cmdline
 bitbake console-image
