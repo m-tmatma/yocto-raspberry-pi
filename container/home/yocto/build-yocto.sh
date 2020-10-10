2dac8003144726923ebbf70f9a141486b99214ab
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-10-10 22:17:30 +0900

    disable  core-image-full-cmdline

--- container/home/yocto/build-yocto.sh
+++ container/home/yocto/build-yocto.sh
@@ -27,6 +27,6 @@ bitbake test-image
 
 find $SCRIPT_DIR/build-rpi/tmp/deploy/sdk -name *.sh | xargs -n 1 -I "{}" sh -c "{} -y"
 
-bitbake core-image-full-cmdline
+#bitbake core-image-full-cmdline
 #bitbake console-image
 
