253ac91e1bdbe04efd7dd91962440e1cb3e84a0c
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-10-11 10:47:12 +0900

    rename nohup.out

--- bg-yocto-build.sh
+++ bg-yocto-build.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-rm -f $SCRIPT_DIR/nohup.out
+
+LOGFILE=$SCRIPT_DIR/nohup.out
+if [ -e $LOGFILE ]; then
+   TIMESTAMP=$(date -r $LOGFILE "+%Y-%m-%d_%H_%M_%S")
+   mv $LOGFILE $LOGFILE.$TIMESTAMP
+fi
+
+rm -f $LOGFILE
 nohup $SCRIPT_DIR/yocto-build.sh $1 &
