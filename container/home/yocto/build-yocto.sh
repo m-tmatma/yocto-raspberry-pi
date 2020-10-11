1164846eeda759f1de48663abed872ef467acd71
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-10-11 09:59:17 +0900

    build populate_sdk.

--- container/home/yocto/build-yocto.sh
+++ container/home/yocto/build-yocto.sh
@@ -23,6 +23,7 @@ fi
 #bitbake core-image-full-cmdline -c populate_sdk
 #bitbake meta-ide-support
 #bitbake meta-toolchain
+bitbake test-image -c populate_sdk
 bitbake test-image
 
 find $SCRIPT_DIR/build-rpi/tmp/deploy/sdk -name *.sh | xargs -n 1 -I "{}" sh -c "{} -y"
