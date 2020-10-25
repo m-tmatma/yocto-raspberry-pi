7797d385e69308ae1b999a8aeca018bcb57d5f00
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-10-25 09:26:23 +0900

    refactoring.

--- repo.sh
+++ repo.sh
@@ -12,7 +12,5 @@ curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ${REPO}
 chmod a+x ${REPO}
 PATH=${REPO_DIR}:$PATH
 
-BRANCHNAME=$(git symbolic-ref --short HEAD)
-REPO_XML_ROOT=$(git rev-parse --show-toplevel)
-repo init -m default.xml -u ${REPO_XML_ROOT} -b ${BRANCHNAME}
+repo init -m default.xml -u $(git rev-parse --show-toplevel) -b $(git rev-parse HEAD)
 repo sync -j4
