b5608c05f8a97c7642eaf0af19ff50592bf61e24
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-01-02 08:36:07 +0900

    refactor DL_DIR.

--- yocto-build.sh
+++ yocto-build.sh
@@ -1,15 +1,19 @@
 #!/bin/sh -e
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
 HOST_DOCKER_HOME=$SCRIPT_DIR/container/home/yocto
-HOST_DL_DIR=${1:-$SCRIPT_DIR/container/home/yocto/downloads}
 TARGET_HOME=/home/yocto
+
 REMOTE_URL=$(git remote get-url origin)
 BRANCH_NAME=$(git name-rev --name-only HEAD | sed "s/\W/_/g")
 CONTAINER_NAME=$(basename $REMOTE_URL)--${BRANCH_NAME}--$(basename $SCRIPT_DIR)__$(git rev-parse --short HEAD)__$(date "+%Y%m%d-%H%M%S")
 
 HOST_DOCKER_OPT=$SCRIPT_DIR/container/opt
 TARGET_OPT=/opt
+
+HOST_DL_DIR=$SCRIPT_DIR/container/home/yocto/downloads
+TARGET_DL_DIR=$TARGET_HOME/downloads
 
 mkdir -p $HOST_DOCKER_HOME
 mkdir -p $HOST_DOCKER_OPT
@@ -18,8 +22,8 @@ mkdir -p $HOST_DL_DIR
 docker run --rm -u yocto:yocto \
 	--name $CONTAINER_NAME \
 	-v $HOST_DOCKER_OPT:$TARGET_OPT \
-	-v $HOST_DL_DIR:$TARGET_HOME/downloads \
-	-v $HOST_DOCKER_HOME:$TARGET_HOME \
+	-v $HOST_DL_DIR:$TARGET_DL_DIR \
+	-v $DOCKER_HOME:$TARGET_HOME \
 	-w $TARGET_HOME yocto-raspberry $TARGET_HOME/build-yocto.sh
 
 ls -Ll $HOST_DOCKER_HOME/build-rpi/tmp/deploy/images/raspberrypi4/*-image-*
--- yocto-shell.sh
+++ yocto-shell.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/sh -e
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
 HOST_DOCKER_HOME=$SCRIPT_DIR/container/home/yocto
-HOST_DL_DIR=${1:-$SCRIPT_DIR/container/home/yocto/downloads}
 TARGET_HOME=/home/yocto
+
 REMOTE_URL=$(git remote get-url origin)
 BRANCH_NAME=$(git name-rev --name-only HEAD | sed "s/\W/_/g")
 CONTAINER_NAME=$(basename $REMOTE_URL)--${BRANCH_NAME}--$(basename $SCRIPT_DIR)__$(git rev-parse --short HEAD)__$(date "+%Y%m%d-%H%M%S")
@@ -11,13 +12,17 @@ CONTAINER_NAME=$(basename $REMOTE_URL)--${BRANCH_NAME}--$(basename $SCRIPT_DIR)_
 HOST_DOCKER_OPT=$SCRIPT_DIR/container/opt
 TARGET_OPT=/opt
 
+HOST_DL_DIR=$SCRIPT_DIR/container/home/yocto/downloads
+TARGET_DL_DIR=$TARGET_HOME/downloads
+
 mkdir -p $HOST_DOCKER_HOME
 mkdir -p $HOST_DOCKER_OPT
 mkdir -p $HOST_DL_DIR
+
 docker run -it --rm -u yocto:yocto \
       --name $CONTAINER_NAME \
       -v $HOST_DOCKER_OPT:$TARGET_OPT \
-      -v $HOST_DL_DIR:$TARGET_HOME/downloads \
+      -v $HOST_DL_DIR:$TARGET_DL_DIR \
       -v $HOST_DOCKER_HOME:$TARGET_HOME \
       -w $TARGET_HOME yocto-raspberry /bin/bash
 
