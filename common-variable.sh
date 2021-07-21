9fc3f3a44974a27a0a98d7b73c94c1c71617b63a
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-07-22 08:18:46 +0900

    change to 20.04

--- common-variable.sh
+++ common-variable.sh
@@ -4,6 +4,6 @@ REMOTE_URL=$(git remote get-url origin)
 IMAGEBASE=$(basename $REMOTE_URL)
 
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
-VERSION=1
+VERSION=ubuntu20.04-2
 DOCKERFILE_MD5=$(md5sum docker-conf/Dockerfile | awk '{ print $1 }')
 DOCKERIMAGE=$IMAGEBASE:$VERSION-$DOCKERFILE_MD5
