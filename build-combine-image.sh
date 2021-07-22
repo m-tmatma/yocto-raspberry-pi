8cde708c19c5969ad5a24aa4cae7836e64b3351a
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-07-22 11:14:13 +0900

    add log.

--- build-combine-image.sh
+++ build-combine-image.sh
@@ -3,4 +3,5 @@
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 source ${SCRIPT_DIR}/common-variable.sh
 
+echo docker build -t $COMBINE_DOCKERIMAGE $COMBINE_DOCKERFILE_DIR
 docker build -t $COMBINE_DOCKERIMAGE $COMBINE_DOCKERFILE_DIR
--- build-toolchain-image.sh
+++ build-toolchain-image.sh
@@ -3,4 +3,5 @@
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 source ${SCRIPT_DIR}/common-variable.sh
 
+echo docker build -t $TOOLCHAIN_DOCKERIMAGE $TOOLCHAIN_DOCKERFILE_DIR
 docker build -t $TOOLCHAIN_DOCKERIMAGE $TOOLCHAIN_DOCKERFILE_DIR
