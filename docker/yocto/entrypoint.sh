00744909e4b05d38faafd2415463946bc8be48c0
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-07-25 13:31:53 +0900

    define $USER

--- docker/yocto/entrypoint.sh
+++ docker/yocto/entrypoint.sh
@@ -8,6 +8,7 @@ HOME_DIR=/home/yocto
 echo "Starting with UID : $USER_ID, GID: $GROUP_ID"
 useradd  -u $USER_ID -o -M $USERNAME
 groupmod -g $GROUP_ID $USERNAME
+export USER=$USERNAME
 export HOME=$HOME_DIR
 
 exec /usr/sbin/gosu $USERNAME "$@"
