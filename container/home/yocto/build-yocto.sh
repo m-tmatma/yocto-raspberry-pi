a5723760b96cc8b446c8582a7c0f9d64e3df5858
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-08-09 07:56:55 +0900

    build custom-test-image

--- container/home/yocto/build-yocto.sh
+++ container/home/yocto/build-yocto.sh
@@ -25,6 +25,5 @@ bitbake meta-toolchain
 
 find $SCRIPT_DIR/build-rpi/tmp/deploy/sdk -name *.sh | xargs -n 1 -I "{}" sh -c "{} -y"
 
-bitbake core-image-full-cmdline
+bitbake custom-test-image
 #bitbake console-image
-
