f6bae384c7d797a0e91ccd85931a4b06460bf1bf
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-08-10 15:45:20 +0900

    build-arg

--- build-image.sh
+++ build-image.sh
@@ -3,5 +3,8 @@
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 source ${SCRIPT_DIR}/common-variable.sh
 
-docker build -t $DOCKERIMAGE $DOCKERFILE_DIR
+docker build \
+    --build-arg http_proxy=$http_proxy \
+    --build-arg https_proxy=$https_proxy \
+    -t $DOCKERIMAGE $DOCKERFILE_DIR
 
